7e9695bec3a32dd16053ea5b657b41c536e4914c
JosafaSilveira <josafajosina@gmail.com>
2023-12-26 18:49:30 -0300

    update style mobile

--- js/index.js
+++ js/index.js
@@ -1,12 +1,16 @@
 // Elementos
-const headerContainer = document.querySelector('.header-container');
-const showMenu = document.querySelector('.menu__actived');
-const menu = document.querySelector('.nav__menu');
-const logo = document.querySelector('#logo');
-const mainContainer = document.querySelector('.main-container');
-const itemMenu = document.querySelectorAll('.nav-menu__link');
+const headerContainer = document.getElementById('header');
+const showMenu = document.getElementById('button__menu');
+const menu = document.getElementById('nav__menu');
+const logo = document.getElementById('logo');
+const mainContainer = document.getElementById('main');
+const links = document.querySelectorAll('.item');
 
-// Função para fechar o menu
+/**
+ * Fecha o menu. Essa função está sendo usada dentro de outra função chamada toggleMenu.
+ * @param {void}
+ * @return {void}
+ */
 const closeMenu = () => {
   menu.style.display = 'none';
   showMenu.style.transform = 'rotate(0deg)';
@@ -15,12 +19,17 @@ const closeMenu = () => {
   logo.style.display = 'block';
 };
 
-// mudar display de menu
+/**
+ * Realiza todas transformações para abrir e fechar o menu como: rotacionar o ícone do botão, aumentar o tamanho do menu, sumir a logo e etc.
+ * @param {void}
+ * @return {void}
+ */
 const toggleMenu = () => {
   if (menu.style.display === 'none') {
     menu.style.display = 'flex';
     showMenu.style.transform = 'rotate(45deg)';
-    headerContainer.style.height = '100%';
+    headerContainer.style.height = '100vh';
+    headerContainer.style.backgroundColor = '#004643';
     mainContainer.style.display = 'none';
     logo.style.display = 'none';
   } else {
@@ -28,11 +37,29 @@ const toggleMenu = () => {
   }
 };
 
-//capturar o click do botão do menu
-showMenu.addEventListener('click', () => {
+/**
+ * Fechar menu o clicar nos links
+ */
+const closeMenuLink = () => {
+  links.forEach((link) => {
+    link.addEventListener('click', () => {
+      document.getElementById('button__menu').click();
+    });
+  });
+};
+
+//Abrir ou fechar o menu ao clicar no botão
+showMenu.addEventListener('click', (e) => {
+  e.preventDefault();
+
   toggleMenu();
 });
 
-itemMenu.addEventListener('click', () => {
-  closeMenu();
+/**
+ * Este evento usa a função closeMenuLink que é usada para fechar o menu quando algum link for clicado.
+ *
+ * Espera todo o documento html ser carregado para rodar a função quando invocada
+ *  */
+document.addEventListener('DOMContentLoaded', () => {
+  closeMenuLink();
 });
